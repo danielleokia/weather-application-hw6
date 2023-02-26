9dd0f360560e553eda6258d89b139395aa93b6d1
unknown <danielleokia1@gmail.com>
2023-02-26 15:14:59 -0800

    added API call for five day forecast and appended cards to the page

--- assets/script1.js
+++ assets/script1.js
@@ -46,40 +46,63 @@ $(document).ready(function () {
             console.log(data)
             var lon = data.coord.lon;
             var lat = data.coord.lat;
-            
+            // API call for longitude and latitude
             $.ajax({
                 type: "GET",
                 url: "http://api.openweathermap.org/geo/1.https://api.openweathermap.org/data/2.5/uvi?appid=5bc72e6052362574738302df8272767f&lat=" + lat + "&lon=" + lon,
-                
-            }).then(function (res){
+
+            }).then(function (res) {
                 console.log(res)
                 var uvColor;
-        var uvResponse = res.value;
-        var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
-        var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
+                var uvResponse = res.value;
+                var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
+                var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
+                //adding classes to the buttons depending on uv responses
+                if (uvResponse < 3) {
+                    btn.addClass("btn-success");
+                } else if (uvResponse < 7) {
+                    btn.addClass("btn-warning");
+                } else {
+                    btn.addClass("btn-danger");
+                }
 
-        if(uvResponse < 3) {
-            btn.addClass("btn-success");
-        } else if (uvResponse < 7) {
-            btn.addClass("btn-warning")
-        }
-
-
-
-
-            }
-        
-                )
-        }
-        )
+                cardBody.append(uvIndex);
+                $("#today .card-body").append(uvIndex.append(btn));
+            });
+            //merge and add responses to the page
+            title.append(img);
+            cardBody.append(title, temp, humid, wind);
+            card.append(cardBody);
+            $("#today").append(card);
+            console.log(data)
+        });
     }
 
+    function fiveDayForecast(searchInput) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=5bc72e6052362574738302df8272767f&units=imperial"
+
+        }).then(function (data) {
+            console.log(data);
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
 
 
-
-
-
-
-
-
-})
+            for (var i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt.txt.indexOf("15:00:00") !== -1) {
+                    var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                    var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                    var colFive = $("<div>").addClass("col-md-2.5");
+                    var cardFive = $("<div>").addClass("card bg-primary text-white");
+                    var cardBodyFive = $("<div>").addClass("card-body p-2");
+                    var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                    var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+                    //merge and display on page
+                    colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, tempFive, humidFive)));
+                    //append card to column, body to card, and other elements to body
+                    $("#forecast .row").append(colFive);
+                }
+            }
+        });
+    }
+});
